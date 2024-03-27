d67ee090f11f328869311c1af5dacead9946ab25
Itohan1 <momoduitohan5@gmail.com>
2024-03-27 16:10:02 -0700

    nu,ber 10

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,7 +7,7 @@
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
-{
+{	
 	ssize_t left_height, right_height;
 
 	if (tree == NULL)
@@ -15,7 +15,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (-1);
+	}
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	return (1 + ((left_height > right_height) ? left_height : right_height));
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
