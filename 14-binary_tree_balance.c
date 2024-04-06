01a62513e2d77012d9c3509c7335ab64d402b8e5
Itohan1 <momoduitohan5@gmail.com>
2024-04-06 09:17:26 -0700

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,27 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - checks the height of the tree
+ * @tree: tree perimeter
+ * Return: left_height
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
  * binary_tree_balance - checks binary tree is balanced
  * @tree: tree root perimeter
  * Return: left_height
@@ -15,25 +36,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->left == NULL && tree->right == NULL)
-	{
-		return (0);
-	}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	if (tree->left != NULL && tree->right == NULL)
-	{
-		return (1);
-	}
-
-	if (tree->left == NULL && tree->right != NULL)
-	{
-		return (-1);
-	}
-
-	if (tree->left != NULL && tree->right != NULL)
-	{
-		left_height = binary_tree_balance(tree->left);
-		right_height = binary_tree_balance(tree->right);
-	}
 	return (left_height - right_height);
 }
