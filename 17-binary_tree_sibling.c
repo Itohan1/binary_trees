f1a1e5124ac6ca21efca33c440a26e9ba2b8f573
Itohan1 <momoduitohan5@gmail.com>
2024-04-06 11:30:35 -0700

    sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,29 +8,24 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent;
-
-	parent = malloc(sizeof(binary_tree_t));
-
-	if (node == NULL || parent == NULL)
+	if (node == NULL || node->parent == NULL)
 	{
 		return (NULL);
 	}
-
-	if (node->parent == NULL)
-	{
-		return (0);
-	}
-
+	
 	if (node->parent->left != NULL && node->parent->left != node)
 	{
 		return (node->parent->left);
 	}
 
-	if (node->parent->right != NULL || node->parent->right != node)
+	if (node->parent->right != NULL && node->parent->right != node)
 	{
 		return (node->parent->right);
 	}
 
+	if (node->parent->left == NULL || node->parent->right == NULL)
+	{
+		return (NULL);
+	}
 	return (node);
 }
