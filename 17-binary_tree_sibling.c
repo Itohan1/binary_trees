f9ea7a32b48f7621358968f90beaafd42fb0196b
Itohan1 <momoduitohan5@gmail.com>
2024-04-06 11:32:51 -0700

    sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	{
 		return (NULL);
 	}
-	
+
 	if (node->parent->left != NULL && node->parent->left != node)
 	{
 		return (node->parent->left);
