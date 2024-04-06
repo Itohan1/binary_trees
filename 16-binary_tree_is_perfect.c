f7632bc6ce4d0bcf6dae33597f624681f1536599
Itohan1 <momoduitohan5@gmail.com>
2024-04-06 10:55:37 -0700

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdio.h>
 
 /**
  * binary_tree_height - checks the height of the tree
@@ -15,10 +16,53 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 
-
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
+ * raise_to_power - gets the power
+ * @base: base
+ * @power: power variable
+ * return: result
+ */
+
+int raise_to_power(int base, int power)
+{
+	int i = 0, result = 1;
+
+	if (power == 0)
+	{
+		return (1);
+	}
+
+	while (i < power)
+	{
+		result *= base;
+		i++;
+	}
+	return (result);
+}
+
+/**
+ * binary_tree_size - checks binary tree size
+ * @tree: tree perimeter
+ * Return: tree->left
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+        if (tree == NULL)
+        {
+                return (0);
+        }
+
+        return (1 + (binary_tree_size(tree->left) + binary_tree_size(tree->right)));
 }
 
 /**
@@ -29,23 +73,21 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int left_depth, right_depth;
+	size_t result, size, final_result;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	left_depth = binary_tree_height(tree->left);
-	right_depth = binary_tree_height(tree->right);
+	result = binary_tree_height(tree) + 1;
+	size = binary_tree_size(tree);
 
-	if (left_depth == right_depth)
+	final_result = raise_to_power(2, result) - 1;
+
+	if (size == final_result)
 	{
-		if (binary_tree_is_perfect(tree->left) &&
-				binary_tree_is_perfect(tree->right))
-		{
-			return (1);
-		}
+		return (1);
 	}
 	return (0);
 }
